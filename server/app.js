b6c16e023771751195f4ae3a1ebb0568f5e3e0fc
imraymondlee <imleeraymond@gmail.com>
2019-11-20 11:51:55 -0800

    Create image upload endpoint

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const app = express();
 
@@ -21,6 +22,26 @@ app.use(
     graphiql: true,
   }),
 );
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname)
+  }
+});
+let upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.send(file);
+});
 
 app.listen(4000, () => {
   console.log('Now listening on port 4000');
