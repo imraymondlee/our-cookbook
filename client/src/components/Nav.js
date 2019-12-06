790590d15b96e1b17ad89e97d68fa69867dfbfd8
imraymondlee <imleeraymond@gmail.com>
2019-12-05 16:11:27 -0800

    Install react-google-login

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+const responseGoogle = (response) => {
+  console.log(response);
+  console.log("GoogleId:", response.googleId);
+}
+
+const logout = () => {
+  console.log('Logging Out');
+}
 
 const Nav = () => {
   return (
@@ -9,6 +19,19 @@ const Nav = () => {
         <Link to="/" className="nav__item">All Recipes</Link>
         <Link to="/new-recipe/" className="nav__item">New Recipe</Link>
         <Link to="/random-recipe/" className="nav__item">Random Recipe</Link>
+        <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENTID}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        <GoogleLogout
+              clientId={process.env.REACT_APP_GOOGLE_CLIENTID}
+              buttonText="Logout"
+              onLogoutSuccess={logout}
+            >
+        </GoogleLogout>
       </div>
     </div>
   );
